98e7ffe848f5111741acf2f261aff32777298645
AnhkyN <anhkynguyen56@gmail.com>
2026-04-08 20:58:35 -0700

    4.8.26 Practice

---
+++ recreated_tables/first and last touch/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE website_events (
+    page_name VARCHAR(50),
+    timestamp DATETIME,
+    user_id INT,
+    utm_campaign VARCHAR(100),
+    utm_source VARCHAR(50)
+);
---
+++ recreated_tables/first and last touch/seed.sql
@@ -0,0 +1,39 @@
+INSERT INTO website_events (page_name, timestamp, user_id, utm_campaign, utm_source) VALUES
+('1 - landing_page', '2018-01-24 03:12:16', 10006, 'getting-to-know-cool-tshirts', 'nytimes'),
+('2 - shopping_cart', '2018-01-24 04:04:16', 10006, 'getting-to-know-cool-tshirts', 'nytimes'),
+('3 - checkout', '2018-01-25 23:10:16', 10006, 'weekly-newsletter', 'email'),
+
+('1 - landing_page', '2018-01-25 20:32:02', 10030, 'ten-crazy-cool-tshirts-facts', 'buzzfeed'),
+('2 - shopping_cart', '2018-01-25 23:05:02', 10030, 'ten-crazy-cool-tshirts-facts', 'buzzfeed'),
+('3 - checkout', '2018-01-28 13:26:02', 10030, 'retargetting-campaign', 'email'),
+('4 - purchase', '2018-01-28 13:38:02', 10030, 'retargetting-campaign', 'email'),
+
+('1 - landing_page', '2018-01-05 18:31:17', 10045, 'getting-to-know-cool-tshirts', 'nytimes'),
+('2 - shopping_cart', '2018-01-05 21:16:17', 10045, 'getting-to-know-cool-tshirts', 'nytimes'),
+('3 - checkout', '2018-01-09 03:05:17', 10045, 'retargetting-ad', 'facebook'),
+
+('1 - landing_page', '2018-01-16 04:17:46', 10048, 'interview-with-cool-tshirts-founder', 'medium'),
+('2 - shopping_cart', '2018-01-16 07:28:46', 10048, 'interview-with-cool-tshirts-founder', 'medium'),
+('3 - checkout', '2018-01-19 00:00:46', 10048, 'retargetting-campaign', 'email'),
+
+('1 - landing_page', '2018-01-02 23:14:01', 10069, 'ten-crazy-cool-tshirts-facts', 'buzzfeed'),
+('2 - shopping_cart', '2018-01-02 23:55:01', 10069, 'ten-crazy-cool-tshirts-facts', 'buzzfeed'),
+('3 - checkout', '2018-01-04 08:12:01', 10069, 'retargetting-ad', 'facebook'),
+('4 - purchase', '2018-01-04 08:13:01', 10069, 'retargetting-ad', 'facebook'),
+
+('1 - landing_page', '2018-01-29 21:37:10', 10162, 'getting-to-know-cool-tshirts', 'nytimes'),
+('2 - shopping_cart', '2018-01-29 22:11:10', 10162, 'ten-crazy-cool-tshirts-facts', 'buzzfeed'),
+('3 - checkout', '2018-02-01 04:15:10', 10162, 'weekly-newsletter', 'email'),
+('4 - purchase', '2018-02-01 04:26:10', 10162, 'weekly-newsletter', 'email'),
+
+('1 - landing_page', '2018-01-24 07:10:33', 10177, 'getting-to-know-cool-tshirts', 'nytimes'),
+('2 - shopping_cart', '2018-01-24 11:58:33', 10177, 'getting-to-know-cool-tshirts', 'nytimes'),
+
+('1 - landing_page', '2018-01-23 22:27:18', 10254, 'interview-with-cool-tshirts-founder', 'medium'),
+('2 - shopping_cart', '2018-01-24 03:18:18', 10254, 'interview-with-cool-tshirts-founder', 'medium'),
+('3 - checkout', '2018-01-25 09:12:18', 10254, 'paid-search', 'google'),
+
+('1 - landing_page', '2018-01-18 05:27:25', 10329, 'interview-with-cool-tshirts-founder', 'medium'),
+('2 - shopping_cart', '2018-01-18 07:15:25', 10329, 'interview-with-cool-tshirts-founder', 'medium'),
+('3 - checkout', '2018-01-22 16:31:25', 10329, 'retargetting-campaign', 'email'),
+('4 - purchase', '2018-01-22 16:35:25', 10329, 'retargetting-campaign', 'email');
